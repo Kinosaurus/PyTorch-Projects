4647abe7cb76cbcd06a40269b3376bee940780bd
Kinosaurus <85656371+Kinosaurus@users.noreply.github.com>
2026-08-02 09:42:20 +0800

    Removed all lr scheduler code from train_step(), shifted them to train()

--- engine.py
+++ engine.py
@@ -13,12 +13,10 @@ def train_step(
   train_dataloader: torch.utils.data.DataLoader,
   optimizer: torch.optim.Optimizer,
   device: torch.device,
-  step_size: int=10,
-  gamma: float=0.1,
   loss_fn: nn.Module=nn.CrossEntropyLoss()) -> Tuple[float, float]:
   """Trains the model for 1 epoch.
 
-  Takes in a train dataloader, optimizer, device and loss function. 
+  Takes in a train dataloader, optimizer, device and loss function.
 
   Returns the average loss and accuracy per training batch.
 
@@ -27,26 +25,20 @@ def train_step(
     train_dataloader (torch.utils.data.DataLoader): A PyTorch DataLoader for the training data.
     optimizer (torch.optim.Optimizer): A PyTorch optimizer to help minimize the loss function.
     device (torch.device): The target device to compute on. (e.g. "cuda" or "cpu").
-    step_size (int): After every step_size epochs, learning rate to decay. Default: 10.
-    gamma (float): Factor for learning rate to drop by. Default: 0.1
     loss_fn (nn.Module): A PyTorch loss function to minimize. Default: nn.CrossEntropyLoss().
 
   Returns:
-    A tuple of training loss, training accuracy metrics and latest learning rate, in the form (train_loss, train_accuracy, learning_rate).
+    A tuple of training loss and training accuracy metrics, in the form (train_loss, train_accuracy).
 
-  Example usage: 
+  Example usage:
     train_step(
       model=model_0,
       train_dataloader=train_dataloader,
       optimizer=optimiser,
       device=device,
       loss_fn=loss_fn,
-      step_size=step_size,
-      gamma=gamma
     )
   """
-  # Setup learning rate scheduler
-  scheduler = lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
   # Activate train mode
   model.train()
   # Setup variables to track and accumulate loss and accuracy
@@ -69,8 +61,7 @@ def train_step(
     loss.backward()
     # Optimiser Step
     optimizer.step()
-    # Update Learning Rate
-    scheduler.step()
+
     # Calculate Acc
     train_labels = train_logits.argmax(dim=1)
     acc = torch.eq(train_labels, y).sum().item()/len(y)
@@ -79,18 +70,16 @@ def train_step(
   # Calculate Average Loss and Accuracy per batch
   avg_loss = total_loss / len(train_dataloader)
   avg_acc = total_acc /len(train_dataloader)
-  # Latest Learning Rate
-  learning_rate = scheduler.get_last_lr()[0]
-  return avg_loss, avg_acc, learning_rate
+  return avg_loss, avg_acc
 
-def test_step( 
+def test_step(
   model: nn.Module,
   test_dataloader: torch.utils.data.DataLoader,
   device: torch.device,
   loss_fn: nn.Module=nn.CrossEntropyLoss()) -> Tuple[float, float]:
   """Evaluates the model for 1 epoch.
 
-  Takes in a test dataloader, device and loss function. 
+  Takes in a test dataloader, device and loss function.
 
   Returns the average loss and accuracy per testing batch.
 
@@ -102,8 +91,8 @@ def test_step(
 
   Returns:
     A tuple of testing loss and testing accuracy metrics, in the form (test_loss, test_accuracy)
-  
-  Example usage: 
+
+  Example usage:
     test_step(
       model=model_0,
       test_dataloader=test_dataloader,
@@ -175,6 +164,8 @@ def train(
       "test_loss": [],
       "test_acc": []
   }
+  # Setup learning rate scheduler
+  scheduler = lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
   # Iterate through epochs
   for epoch in tqdm(range(epochs)):
     # Training
@@ -194,6 +185,10 @@ def train(
       device=device,
       loss_fn=loss_fn
     )
+    # Update Learning Rate
+    scheduler.step()
+    # Latest Learning Rate
+    learning_rate = scheduler.get_last_lr()[0]
     # Visualise results for this epoch
     print(f"Epoch: {epoch+1} | Learning Rate: {learning_rate} | Train Loss: {train_loss:.4f} | Train Accuracy: {train_acc:.4f} | Test Loss: {test_loss:.4f} | Test Accuracy {test_acc:.4f}")
     # Update results dict
